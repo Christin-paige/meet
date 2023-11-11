0fb964352590ef51c07ab58ca63728427e934079
Christin-paige <christinpmartin@gmail.com>
2023-11-11 12:48:56 -0500

    updating code to troubleshoot deployment

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,36 +1,22 @@
 
 import { useState } from 'react';
-import mockData  from '../mock-data';
 
 
 const Event = ({ event }) => {
   const [showDetails, setShowDetails] = useState(false);
- 
-  const toggleDetails = () => {
-    setShowDetails(!showDetails);
-  };
-
-  return ( 
-    <li className="eventTitle">
-    <div id="eventList">{event.summary}</div>
-    <div>{event.created}</div>
-    <div>{event.location}</div>
-   
-
-<div className="detailsBtn">
- <button
-   className="showDetailsButton"
-  
-   onClick={toggleDetails}
-   >show details
-   </button>
- <button
-   className="hideDetails"
-   >hide details</button>
-</div>
-
-   {showDetails && <div className="eventDetails">{event.description}</div>}
-  </li>
+  return (
+    <li className="event">
+      <h2>{event && event.summary}</h2>
+      <p>{event && event.location}</p>
+      <p>{event && (new Date(event.created)).toUTCString()}</p>
+      {showDetails ?
+        <p className="details">{event && event.description}</p> :
+        null
+      }
+      <button className="details-btn" onClick={() => {
+        showDetails ? setShowDetails(false) : setShowDetails(true)
+      }}>{showDetails ? "hide details" : "show details"}</button>
+    </li>
   )
 }
 
--- src/components/NumberOfEvents.js
+++ src/components/NumberOfEvents.js
@@ -1,19 +1,28 @@
+// src/components/NumberOfEvents.js
 
+import { useState } from "react";
 
+const NumberOfEvents = ({ }) => {
 
-const NumberOfEvents = () => {
+  const [number, setNumber] = useState(32);
 
+  const handleInputChanged = (event) => {
+    const value = event.target.value;
+    setNumber(value);
+  }
 
-
-return (
-    <div id='numberOfEvents'>
+  return (
+    <div id="number-of-events">
+      <label htmlFor="number-of-events-input">Number of Events: </label>
       <input
-      type="text"
-      className="defaultValue"
-      defaultValue={32}
+        type="text"
+        id="number-of-events-input"
+        className="number-of-events-input"
+        value={number}
+        onChange={handleInputChanged}
       />
     </div>
-
-)}
+  );
+}
 
 export default NumberOfEvents;

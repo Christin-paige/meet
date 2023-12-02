affd9154ea2a0f7b1e5c01ab38885732ec3d90d5
Christin-paige <christinpmartin@gmail.com>
2023-12-02 10:48:51 -0500

    added scatterchart

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import NumberOfEvents from './components/NumberOfEvents';
 import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
+import CityEventsChart from './components/CityEventsChart';
 import { useEffect, useState } from 'react';
 import { extractLocations, getEvents } from './api';
 import { InfoAlert } from './components/Alert';
@@ -42,6 +43,7 @@ const fetchData = async () => {
 
  return (
    <div className="App">
+   <h1>Meet App</h1>
     <div className="alerts-container">
         {infoAlert.length ? <InfoAlert text={infoAlert}/> : null}
         {errorAlert.length ? <ErrorAlert text={errorAlert}/> : null}
@@ -54,6 +56,7 @@ const fetchData = async () => {
      <NumberOfEvents setCurrentNOE={setCurrentNOE}
       setErrorAlert={setErrorAlert}
       />
+      <CityEventsChart allLocations={allLocations} events={events} />
      <EventList events={events}/>
    </div>
  );
